f6d6d1362c01f19162337697f9517113ee5fa86f
aidynfatikh <aidyn.fatikh@gmail.com>
2025-06-15 14:49:32 +0500

    fix

--- front/src/api.ts
+++ front/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Task, TaskCreate, UserLogin, Token, UserSignup } from './types';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Add a request interceptor to include the token
 axios.interceptors.request.use((config) => {
